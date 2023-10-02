a6eab4a968c367905d4b2254e8f6e589617fb011
aquimo <aquimoadriano@gmail.com>
2023-10-02 15:38:45 +0200

    Files 2

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj (any): object of any type
+
+    Returns:
+        list of available attributes and methods
+
+    """
+    return dir(obj)
