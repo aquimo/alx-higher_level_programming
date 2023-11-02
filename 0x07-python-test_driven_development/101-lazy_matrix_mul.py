f1b378d3c02a8699f40fd89ffa022a72d031fa69
aquimo <aquimoadriano@gmail.com>
2023-11-02 11:51:21 +0200

    Defer

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 101-lazy_matrix_mul.py
-import numpy as np
-
-
-def lazy_matrix_mul(m_a, m_b):
-
-
-    return (np.matmul(m_a, m_b))
--- 0x0A-python-inheritance/README.md
+++
@@ -1,7 +0,0 @@
-0x0A. Python - Inheritance
-
-0. Lookup
-
-1. My list
-
-2. Exact same object
