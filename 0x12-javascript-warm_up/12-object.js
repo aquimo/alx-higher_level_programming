d7ef418259d05e884162f1b0805cc55e7a414827
aquimo <aquimoadriano@gmail.com>
2024-02-14 11:11:34 +0200

    Files

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// edit existing code (insert line 8) to replace 12 with 89
+// edit code (insert line 8) to replace 12 with 89
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-myObject.value = 89; // added this line
+myObject.value = 89; // add this line
 console.log(myObject);
