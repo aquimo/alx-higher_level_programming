15c7e5ad27283e452f1d35b95b1c5011e2c2d539
aquimo <aquimoadriano@gmail.com>
2024-03-01 11:58:59 +0200

    Us

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@
 if __name__ == "__main__":
     from urllib import request
 
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
     print('Body response:')
     print('\t- type: {}'.format(type(html)))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@
 if __name__ == "__main__":
     from requests import get
 
-    response = get('https://intranet.hbtn.io/status')
+    response = get('https://alx-intranet.hbtn.io/status')
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(
         type(response.text), response.text))
