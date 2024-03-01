ca473172fc1db3f834dd35a831afa0ee43a37e22
aquimo <aquimoadriano@gmail.com>
2024-03-01 12:07:27 +0200

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """0x11. Python - Network #1
 """
+import requests
+import sys
+
+def display_response_body(url):
+    try:
+        response = requests.get(url)
+        if response.status_code >= 400:
+            print(f"Error code: {response.status_code}")
+        else:
+            print(response.text)
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
 
 if __name__ == "__main__":
-    from requests import get
-    from sys import argv
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
 
-    try:
-        response = get(argv[1])
-        # override defualt handling of exceptions and reraise them
-        response.raise_for_status()
-    except:
-        print('Error code: {}'.format(response.status_code))
-    else:
-        print(response.text)
+    url = sys.argv[1]
+    display_response_body(url)
+    
